be416c7ac7bef9194ef6b94074b10b06cba34d97
storm1kk <storm1kk@gmail.com>
2024-07-23 12:04:47 +0000

    add map command

--- repl.go
+++ repl.go
@@ -45,6 +45,11 @@ func getCommands() map[string]cliCommand {
 			description: "Prints the help menu",
 			callback:    callbackHelp,
 		},
+		"map": {
+			name:        "map",
+			description: "Lists some location areas",
+			callback:    callbackMap,
+		},
 		"exit": {
 			name:        "exit",
 			description: "Exits the pokedex",
